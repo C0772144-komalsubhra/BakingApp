e7e48170a57bbf600da7df79e8036949c65ecd9c
komalcuty40@gmail.com <komalsubhra6768@gmail.com>
2020-07-02 19:35:38 -0400

    creating adapters

--- app/src/main/java/com/example/myapplication/ui/adapter/IngredientsAdapter.java
+++ app/src/main/java/com/example/myapplication/ui/adapter/IngredientsAdapter.java
@@ -1,4 +1,63 @@
 package com.example.myapplication.ui.adapter;
 
-public class IngredientsAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.model.Ingredients;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsAdapter.ViewHolder> {
+    private ArrayList<Ingredients> listItems;
+    private Context context;
+
+    public IngredientsAdapter(Context context, ArrayList<Ingredients> listItems) {
+        this.context = context;
+        this.listItems = listItems;
+    }
+
+    @NonNull
+    @Override
+    public IngredientsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).
+                inflate(R.layout.row_ingredients, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int position) {
+        viewHolder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.quantity)
+        TextView quantity;
+        @BindView(R.id.desc) TextView description;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+        void bind(int position) {
+            Ingredients ingredient = listItems.get(position);
+            quantity.setText(ingredient.getQuantity() + " " + ingredient.getMeasure());
+            description.setText(ingredient.getIngredient());
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.java
@@ -1,4 +1,32 @@
 package com.example.myapplication.viewmodel;
 
-public class MainViewModel {
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.myapplication.model.ListItem;
+import com.example.myapplication.repo.RecipeRepo;
+
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private RecipeRepo recipeRepo;
+    private LiveData<List<ListItem>> recipeData;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        recipeRepo = new RecipeRepo(application);
+
+        if (recipeData == null) {
+            Log.d("Recipe", "getContentFromAPI: Called It's null");
+            recipeData = recipeRepo.getContentFromAPI();
+        }
+
+    }
+
+    public LiveData<List<ListItem>> getContentFromAPI(){ return recipeData; }
 }

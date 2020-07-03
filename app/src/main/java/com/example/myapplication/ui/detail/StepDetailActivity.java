9fc4f0f85d3e3ef26825941d67624969813d5b90
komalcuty40@gmail.com <komalsubhra6768@gmail.com>
2020-07-03 18:38:30 -0400

    add fallback image for video

--- app/src/main/java/com/example/myapplication/ui/detail/StepDetailActivity.java
+++ app/src/main/java/com/example/myapplication/ui/detail/StepDetailActivity.java
@@ -21,7 +21,7 @@ public class StepDetailActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_step_list);
+        setContentView(R.layout.activity_step_detail);
         setTitle("Steps");
 
         ArrayList<Steps> stepsList = new ArrayList<>();

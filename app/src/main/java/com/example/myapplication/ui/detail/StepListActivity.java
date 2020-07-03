9563e69aaad5d35aa5eae2ef2e4761a54fe3b4b3
komalcuty40@gmail.com <komalsubhra6768@gmail.com>
2020-07-03 18:21:17 -0400

    adding fragments

--- app/src/main/java/com/example/myapplication/ui/detail/StepListActivity.java
+++ app/src/main/java/com/example/myapplication/ui/detail/StepListActivity.java
@@ -1,4 +1,56 @@
 package com.example.myapplication.ui.detail;
 
-public class StepListActivity {
+import android.os.Bundle;
+import android.os.Parcelable;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+import com.example.myapplication.utils.Constants;
+
+import java.util.ArrayList;
+
+public class StepListActivity  extends AppCompatActivity {
+
+    private boolean mTwoPane;
+    private ArrayList<Parcelable> steps = new ArrayList<>();
+    private ArrayList<Parcelable> ingredients = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_list);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mTwoPane = findViewById(R.id.two_pane_layout_root) != null;
+
+        if (getIntent().getExtras() != null) {
+            steps = getIntent().getParcelableArrayListExtra(Constants.STEPS);
+            ingredients = getIntent().getParcelableArrayListExtra(Constants.INGREDIENTS);
+            setTitle(getIntent().getStringExtra(Constants.RECIPE_NAME));
+        }
+
+        if (mTwoPane) {
+            replaceFragment(
+                    StepListFragment.newInstance(steps, ingredients),
+                    R.id.step_container
+            );
+
+        } else {
+            replaceFragment(
+                    StepListFragment.newInstance(steps, ingredients),
+                    R.id.container
+            );
+        }
+    }
+
+    private void replaceFragment(Fragment fragment, int id) {
+        if (steps.size() != 0 && ingredients.size() != 0) {
+            //Fragment fragment = StepListFragment.newInstance(steps, ingredients);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(id, fragment)
+                    .commit();
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/ui/detail/StepListFragment.java
+++ app/src/main/java/com/example/myapplication/ui/detail/StepListFragment.java
@@ -1,4 +1,120 @@
 package com.example.myapplication.ui.detail;
 
-public class StepListFragment {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.model.Ingredients;
+import com.example.myapplication.model.Steps;
+import com.example.myapplication.ui.adapter.IngredientsAdapter;
+import com.example.myapplication.ui.adapter.StepsAdapter;
+import com.example.myapplication.utils.Constants;
+import com.example.myapplication.utils.ItemClickSupport;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class StepListFragment extends Fragment {
+
+    @BindView(R.id.ingredientsList)
+    RecyclerView ingredientsRcl;
+    @BindView(R.id.stepsList) RecyclerView stepsRcl;
+
+    private ArrayList<Steps> stepsList = new ArrayList<>();
+    private ArrayList<Ingredients> ingredientsList = new ArrayList<>();
+
+    boolean mTwoPane;
+    public StepListFragment() { }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_step_list, container, false);
+        ButterKnife.bind(this, view);
+        readBundle(getArguments());
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState!=null){
+            if (mTwoPane)
+                getActivity().findViewById(R.id.select_step).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupViews();
+
+        mTwoPane = getActivity().findViewById(R.id.two_pane_layout_root) !=null;
+        setListener();
+    }
+
+    private void setListener() {
+        ItemClickSupport.addTo(stepsRcl).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                if (!mTwoPane) {
+                    Intent intent = new Intent(getActivity(), StepDetailActivity.class);
+                    intent.putExtra(Constants.STEPS, stepsList);
+                    intent.putExtra(Constants.POSITION, position);
+
+                    startActivity(intent);
+                }else {
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.detail_container,
+                                    StepDetailFragment.newInstance(stepsList, position))
+                            .commit();
+
+                    getActivity().findViewById(R.id.select_step).setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    private void setupViews() {
+        ingredientsRcl.setLayoutManager(new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.HORIZONTAL, false));
+
+        IngredientsAdapter adapter = new IngredientsAdapter(getActivity(), ingredientsList);
+        ingredientsRcl.setAdapter(adapter);
+
+        stepsRcl.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        StepsAdapter stepsAdapter = new StepsAdapter(getActivity(), stepsList);
+        stepsRcl.setAdapter(stepsAdapter);
+    }
+
+    private void readBundle(Bundle bundle) {
+        if (bundle != null) {
+            stepsList = bundle.getParcelableArrayList(Constants.STEPS);
+            ingredientsList = bundle.getParcelableArrayList(Constants.INGREDIENTS);
+        }
+    }
+
+    public static StepListFragment newInstance(ArrayList<Parcelable> steps,
+                                               ArrayList<Parcelable> ingredients) {
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList(Constants.STEPS, steps);
+        bundle.putParcelableArrayList(Constants.INGREDIENTS, ingredients);
+        StepListFragment fragment = new StepListFragment();
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
 }
